1074e5c28b2b2ea0b40226693b71582460256e16
Yclin <lyichuan@gmail.com>
2016-06-12 22:09:18 +0200

    tipcalc RecyclerView Layout

---
+++ app/src/main/java/edu/galileo/android/tipcalc/activities/TipDetailActivity.java
@@ -0,0 +1,15 @@
+package edu.galileo.android.tipcalc.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.galileo.android.tipcalc.R;
+
+public class TipDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_detail);
+    }
+}
